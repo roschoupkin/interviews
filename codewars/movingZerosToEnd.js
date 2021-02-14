b732f58619f621b9fa1471da0de319543d81a680
Daniil Roschoupkin <d.roschoupkin@corp.mail.ru>
2021-02-15 00:45:07 +0300

    Add move zeros to the end

---
+++ codewars/movingZerosToEnd.js
@@ -0,0 +1,18 @@
+/**
+ * Write an algorithm that takes an array and moves all of the zeros to the end, preserving the order of the other elements.
+ * moveZeros([false,1,0,1,2,0,1,3,"a"]) // [false,1,1,2,1,3,"a",0,0]
+ */
+
+const moveZeros = (arr) => {
+  const filtered = arr.filter(v => v !== 0);
+  return [...filtered, ...Array(arr.length - filtered.length).fill(0)];
+}
+
+const moveZeros2 = (arr) => {
+  return arr
+    .filter((val) => val !== 0)
+    .concat(arr.filter((val) => val === 0));
+}
+
+moveZeros([false,1,0,1,2,0,1,3,"a"]) // [false,1,1,2,1,3,"a",0,0]
+moveZeros2([false,1,0,1,2,0,1,3,"a"]) // [false,1,1,2,1,3,"a",0,0]
