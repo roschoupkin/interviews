0851586ac62680a0e2e55aef8dec1bfafc32f43f
Daniil Roschoupkin <d.roschoupkin@corp.mail.ru>
2021-02-15 00:51:34 +0300

    Add recoverSecret and stringToCamelCase tasks

---
+++ codewars/recoverSecret.js
@@ -0,0 +1,31 @@
+/**
+ * There is a secret string which is unknown to you. Given a collection of random triplets from the string, recover the original string.
+ * A triplet here is defined as a sequence of three letters such that each letter occurs somewhere before the next in the given string. "whi" is a triplet for the string "whatisup".
+ * As a simplification, you may assume that no letter occurs more than once in the secret string.
+ * You can assume nothing about the triplets given to you other than that they are valid triplets and that they contain sufficient information to deduce the original string. In particular, this means that the secret string will never contain letters that do not occur in one of the triplets given to you.
+ */
+
+const recoverSecret = function(triplets) {
+  const secret = [];
+  while(triplets.filter(t => t.length).length) {
+    for (const [letter] of triplets) {
+      if (triplets.every(triplet => triplet.indexOf(letter) <= 0)) {
+        secret.push(letter);
+        for (const triplet of triplets.filter(t => t[0] === letter)) {
+          triplet.shift()
+        }
+      }
+    }
+  }
+  return secret.join('');
+}
+
+recoverSecret([
+  ['t','u','p'],
+  ['w','h','i'],
+  ['t','s','u'],
+  ['a','t','s'],
+  ['h','a','p'],
+  ['t','i','s'],
+  ['w','h','s']
+]); // whatisup
---
+++ codewars/stringToCamelCase.js
@@ -0,0 +1,29 @@
+/**
+ * Complete the method/function so that it converts dash/underscore delimited words into camel casing. The first word within the output should be capitalized only if the original word was capitalized (known as Upper Camel Case, also often referred to as Pascal case).
+ * toCamelCase("the-stealth-warrior") // returns "theStealthWarrior"
+ * toCamelCase("The_Stealth_Warrior") // returns "TheStealthWarrior"
+ */
+
+function toCamelCase(str){
+  return str.split(/[\_||-]/g).reduce((s, v, i) => {
+    if (i === 0) {
+      return s + v;
+    }
+    return s + v.charAt(0).toUpperCase() + v.slice(1, v.length);
+  }, '');
+}
+
+function toCamelCase2(str){
+  const regExp=/[-_]\w/ig;
+  return str.replace(regExp,function(match){
+    return match.charAt(1).toUpperCase();
+  });
+}
+
+function toCamelCase3(str){
+  return str.replace(/[-_](.)/g, (_, c) => c.toUpperCase());
+}
+
+toCamelCase("the-stealth-warrior") // returns "theStealthWarrior"
+toCamelCase2("The_Stealth_Warrior") // returns "TheStealthWarrior"
+toCamelCase3("The_Stealth-warrior") // returns "TheStealthWarrior"
