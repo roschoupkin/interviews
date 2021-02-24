9ba9604f914fb82cc9c58518082f6da12fd82509
Daniil Roschoupkin <d.roschoupkin@corp.mail.ru>
2021-02-24 13:13:59 +0300

    Update count chars

--- other/countChars.js
+++ other/countChars.js
@@ -1,5 +1,19 @@
+/**
+ * Дана строка (возможно, пустая), состоящая из букв A-Z:
+ * AAAABBBCCXYZDDDDEEEFFFAAAAAABBBBBBBBBBBBBBBBBBBBBBBBBBBB
+ * Нужно написать функцию counter, которая на выходе даст строку вида:
+ * A4B3C2XYZD4E3F3A6B28
+ * Пояснения:
+ * - Если символ встречается 1 раз, он остается без изменений;
+ * - Если символ повторяется более 1 раза, к нему добавляется количество повторений.
+ */
 function counter(str) {
-  return str.replace(/([a-z])\1+/g, (m) => `${m[0]}${m.length}`);
+  if (/[^A-Z]/g.test(str)) {
+    throw Error('Invalid string');
+  }
+  return str.replace(/([A-Z])\1+/g, (m) => `${m[0]}${m.length}`);
 }
 
-console.log(counter('aaaaasssddffghh')); // a5s3d2f2gh2
+console.log(counter('AAAABBBCCXYZDDDDEEEFFFAAAAAABBBBBBBBBBBBBBBBBBBBBBBBBBBB')); // A4B3C2XYZD4E3F3A6B28
+console.log(counter('AAAABBBCCXYZDDDDEEEfffFFFAAAAAABBBBBBBBBBBBBBBBBBBBBBBBBBBB')); // Error: Invalid string
+
