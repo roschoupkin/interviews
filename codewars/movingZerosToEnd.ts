b9c08a37fed00b6464f5f5d1e68cb0aa2fd6639b
Daniil Roschoupkin <d.roschoupkin@corp.mail.ru>
2021-03-01 01:01:02 +0300

    Add moving zeros to the end

--- codewars/movingZerosToEnd.ts
+++ codewars/movingZerosToEnd.ts
@@ -3,12 +3,12 @@
  * moveZeros([false,1,0,1,2,0,1,3,"a"]) // [false,1,1,2,1,3,"a",0,0]
  */
 
-const moveZeros = (arr) => {
+export const moveZeros = (arr: unknown[]): unknown[] => {
   const filtered = arr.filter((v) => v !== 0);
   return [...filtered, ...Array(arr.length - filtered.length).fill(0)];
 };
 
-const moveZeros2 = (arr) => {
+export const moveZeros2 = (arr: unknown[]): unknown[] => {
   return arr.filter((val) => val !== 0).concat(arr.filter((val) => val === 0));
 };
 
