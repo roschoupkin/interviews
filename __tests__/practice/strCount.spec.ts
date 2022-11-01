653492a8d885e056e5948ed33616b89f2586164e
Daniil Roschoupkin <d.roschoupkin@corp.mail.ru>
2022-11-01 09:41:39 +0400

    Add eventLoop task. Fix tests for strCount

--- __tests__/practice/strCount.spec.ts
+++ __tests__/practice/strCount.spec.ts
@@ -5,10 +5,14 @@ describe('Find substring count into string', () => {
     expect(strCountByRegExp('Hello', 'o')).toBe(1);
     expect(strCountByRegExp('Hello', 'l')).toBe(2);
     expect(strCountByRegExp('', 'z')).toBe(0);
+    expect(strCountByRegExp('hello hello help', 'hello')).toBe(2);
+    expect(strCountByRegExp('hello hello help', 'hel')).toBe(3);
   });
   it('Should return count of substring into string. Fast method', () => {
     expect(strCountFast('Hello', 'o')).toBe(1);
     expect(strCountFast('Hello', 'l')).toBe(2);
     expect(strCountFast('', 'z')).toBe(0);
+    expect(strCountFast('hello hello help', 'hel')).toBe(3);
+    expect(strCountFast('hello hello help', 'hel')).toBe(3);
   });
 });
---
+++ practice/eventLoop.ts
@@ -0,0 +1,18 @@
+console.log('script start');
+
+setTimeout(function () {
+  console.log('setTimeout');
+}, 0);
+
+Promise.resolve()
+  .then(function () {
+    console.log('promise1');
+  })
+  .then(function () {
+    console.log('promise2');
+  });
+
+console.log('script end');
+
+// V8: script start, script end, promise1, promise2, setTimeout
+// Edge or Safari@8 or Firefox@40: script start, script end, setTimeout, promise1, promise2
