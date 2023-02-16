1097279cc4f1d9dd96cca7e996f927dc1fd67277
Daniil Roschupkin <d.roschoupkin@corp.mail.ru>
2023-02-16 15:16:15 +0300

    Add detourRooms task

---
+++ __tests__/practice/detourRooms.spec.ts
@@ -0,0 +1,19 @@
+import { detourRooms1, detourRooms2 } from '@practice/detourRooms';
+
+describe('Detour rooms', () => {
+  it('Should return correct answer by the first solution', () => {
+    expect(detourRooms1([[], [1, 2], []])).toBeFalsy();
+    expect(detourRooms1([[1], [2], [], []])).toBeFalsy();
+    expect(detourRooms1([[1, 2], [], []])).toBeTruthy();
+    expect(detourRooms1([[1, 2], [3], [], []])).toBeTruthy();
+    expect(detourRooms1([[1, 2], [2], [1, 3], [4], [1]])).toBeTruthy();
+  });
+
+  it('Should return correct answer by the second solution', () => {
+    expect(detourRooms2([[], [1, 2], []])).toBeFalsy();
+    expect(detourRooms2([[1], [2], [], []])).toBeFalsy();
+    expect(detourRooms2([[1, 2], [], []])).toBeTruthy();
+    expect(detourRooms2([[1, 2], [3], [], []])).toBeTruthy();
+    expect(detourRooms2([[1, 2], [2], [1, 3], [4], [1]])).toBeTruthy();
+  });
+});
