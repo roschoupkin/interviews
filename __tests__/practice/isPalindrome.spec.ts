66ee60391de60189173129cc8d557e895df5dcad
Daniil Roschupkin <d.roschoupkin@corp.mail.ru>
2023-02-19 18:05:37 +0300

    Add isPalinfrom solution

---
+++ __tests__/practice/isPalindrome.spec.ts
@@ -0,0 +1,27 @@
+import { isPalindrome, isPalindrome2 } from '@practice/isPalindrome';
+
+describe('is palindrome', () => {
+  it('Detect palindrome correctly by the first solution', () => {
+    expect(isPalindrome('Казак')).toBeTruthy();
+    expect(isPalindrome('А роза упала на лапу Азора')).toBeTruthy();
+    expect(isPalindrome('Do geese see God?')).toBeTruthy();
+    expect(isPalindrome('Madam, I’m Adam')).toBeTruthy();
+
+    expect(isPalindrome('Test palindrome')).toBeFalsy();
+    expect(isPalindrome('Тест палиндрома')).toBeFalsy();
+    expect(isPalindrome('I’m not a palindrome')).toBeFalsy();
+    expect(isPalindrome('Я не палиндром')).toBeFalsy();
+  });
+
+  it('Detect palindrome correctly by the second solution', () => {
+    expect(isPalindrome2('Казак')).toBeTruthy();
+    expect(isPalindrome2('А роза упала на лапу Азора')).toBeTruthy();
+    expect(isPalindrome2('Do geese see God?')).toBeTruthy();
+    expect(isPalindrome2('Madam, I’m Adam')).toBeTruthy();
+
+    expect(isPalindrome2('Test palindrome')).toBeFalsy();
+    expect(isPalindrome2('Тест палиндрома')).toBeFalsy();
+    expect(isPalindrome2('I’m not a palindrome')).toBeFalsy();
+    expect(isPalindrome2('Я не палиндром')).toBeFalsy();
+  });
+});
