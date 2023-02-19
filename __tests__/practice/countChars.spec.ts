f965f345829d5877b6d5faf1dec82e39f8aa8749
Daniil Roschupkin <d.roschoupkin@corp.mail.ru>
2023-02-19 18:50:42 +0300

    Add one more RLE solution

--- __tests__/practice/countChars.spec.ts
+++ __tests__/practice/countChars.spec.ts
@@ -1,4 +1,4 @@
-import { counter } from '@practice/countChars';
+import { counter, counter2 } from '@practice/countChars';
 
 describe('Count chars into string', () => {
   it('Should return a new string with counted characters', () => {
@@ -6,5 +6,12 @@ describe('Count chars into string', () => {
   });
   it('Should return error', () => {
     expect(() => counter('AAAABBBCCXYZDDDDEEEfffFFFAAAAAABBBBBBBBBBBBBBBBBBBBBBBBBBBB')).toThrow('Invalid string');
+  });
+
+  it('Should return a new string with counted characters', () => {
+    expect(counter2('AAAABBBCCXYZDDDDEEEFFFAAAAAABBBBBBBBBBBBBBBBBBBBBBBBBBBB')).toBe('A4B3C2XYZD4E3F3A6B28');
+  });
+  it('Should return error', () => {
+    expect(() => counter2('AAAABBBCCXYZDDDDEEEfffFFFAAAAAABBBBBBBBBBBBBBBBBBBBBBBBBBBB')).toThrow('Invalid string');
   });
 });
--- practice/countChars.ts
+++ practice/countChars.ts
@@ -14,3 +14,27 @@ export function counter(str: string): string {
   }
   return str.replace(/([A-Z])\1+/g, (m) => `${m[0]}${m.length}`);
 }
+
+export function counter2(income: string): string {
+  if (/[^A-Z]/.test(income)) {
+    throw new Error('Invalid string');
+  }
+
+  const environment = { prev: income[0], amount: 0, result: '' };
+
+  for (let i = 0; i <= income.length; i++) {
+    const char = income[i];
+
+    if (environment.prev === char) {
+      environment.amount++;
+    } else {
+      const amount = environment.amount === 1 ? '' : environment.amount;
+      environment.result += environment.prev + amount;
+
+      environment.prev = char;
+      environment.amount = 1;
+    }
+  }
+
+  return environment.result;
+}
