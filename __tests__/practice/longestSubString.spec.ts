213278d969402b771ddd149f1920d4f0c9abafd2
Daniil Roschupkin <d.roschoupkin@corp.mail.ru>
2023-02-20 23:43:44 +0300

    Add new method for lastSubString

--- __tests__/practice/longestSubString.spec.ts
+++ __tests__/practice/longestSubString.spec.ts
@@ -1,4 +1,4 @@
-import { longestSubString } from '@practice/longestSubString';
+import { longestSubString, longestSubString2 } from '@practice/longestSubString';
 
 describe('Longest substring', () => {
   it('Should return size of longest substring', () => {
@@ -6,5 +6,12 @@ describe('Longest substring', () => {
     expect(longestSubString('bbbbb')).toBe(1);
     expect(longestSubString('zabcade')).toBe(5);
     expect(longestSubString('abcdebaabcdefg')).toBe(7);
+  });
+
+  it('Should return size of longest substring', () => {
+    expect(longestSubString2('abcabcbb')).toBe(3);
+    expect(longestSubString2('bbbbb')).toBe(1);
+    expect(longestSubString2('zabcade')).toBe(5);
+    expect(longestSubString2('abcdebaabcdefg')).toBe(7);
   });
 });
--- practice/longestSubString.ts
+++ practice/longestSubString.ts
@@ -19,3 +19,21 @@ export function longestSubString(str: string): number {
 
   return longest;
 }
+
+export function longestSubString2(input: string): number {
+  let longest = 0;
+  const store: string[] = [];
+
+  for (let i = 0; i < input.length; i++) {
+    const char = input[i];
+    const last = store.indexOf(char);
+
+    if (last >= 0) {
+      store.splice(0, last + 1);
+    }
+
+    longest = Math.max(longest, store.push(char));
+  }
+
+  return longest;
+}
