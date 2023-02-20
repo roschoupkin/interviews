b46a0b6b5e16ec8eeda51eb6010ebb1fee784897
Daniil Roschupkin <d.roschoupkin@corp.mail.ru>
2023-02-20 23:00:38 +0300

    Add one more solution for countChars

--- __tests__/practice/countChars.spec.ts
+++ __tests__/practice/countChars.spec.ts
@@ -1,4 +1,4 @@
-import { counter, counter2 } from '@practice/countChars';
+import { counter, counter2, counter3 } from '@practice/countChars';
 
 describe('Count chars into string', () => {
   it('Should return a new string with counted characters', () => {
@@ -13,5 +13,12 @@ describe('Count chars into string', () => {
   });
   it('Should return error', () => {
     expect(() => counter2('AAAABBBCCXYZDDDDEEEfffFFFAAAAAABBBBBBBBBBBBBBBBBBBBBBBBBBBB')).toThrow('Invalid string');
+  });
+
+  it('Should return a new string with counted characters', () => {
+    expect(counter3('AAAABBBCCXYZDDDDEEEFFFAAAAAABBBBBBBBBBBBBBBBBBBBBBBBBBBB')).toBe('A4B3C2XYZD4E3F3A6B28');
+  });
+  it('Should return error', () => {
+    expect(() => counter3('AAAABBBCCXYZDDDDEEEfffFFFAAAAAABBBBBBBBBBBBBBBBBBBBBBBBBBBB')).toThrow('Invalid string');
   });
 });
--- practice/countChars.ts
+++ practice/countChars.ts
@@ -38,3 +38,26 @@ export function counter2(income: string): string {
 
   return environment.result;
 }
+
+export function counter3(input: string): string {
+  if (/[^A-Z]/.test(input)) {
+    throw new Error('Invalid string');
+  }
+
+  let result = '';
+  let counter = 1;
+  let current = input[0];
+
+  for (let i = 1; i < input.length; i++) {
+    while (current === input[i]) {
+      counter++;
+      i++;
+    }
+
+    result += current + (counter > 1 ? counter : '');
+    counter = 1;
+    current = input[i];
+  }
+
+  return result;
+}
